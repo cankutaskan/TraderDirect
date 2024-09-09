9cc83429aa59edd5d502e3de7dabb1a1daa7de00
Mustafa Cankut Askan <m.askan@cashpoint.com>
2024-09-09 22:06:10 +0200

    Containerize console app

--- TradesMessageConsumer/Program.cs
+++ TradesMessageConsumer/Program.cs
@@ -1,8 +1,42 @@
-﻿using TradesMessageConsumer;
+﻿using Microsoft.Extensions.Configuration;
+using RabbitMQ.Client.Exceptions;
+using TradesMessageConsumer;
 
-string rabbitMqConnectionString = "amqp://guest:guest@localhost:5672/";
-string queueName = "trades_executed";
-RabbitMqConsumer consumer = new(queueName, rabbitMqConnectionString);
+
+var config = new ConfigurationBuilder()
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddEnvironmentVariables()
+        .Build();
+
+string? cs = config.GetSection("RabbitMq:ConnectionString").Value;
+string? queueName = config.GetSection("RabbitMq:QueueName").Value;
+
+Console.WriteLine($"ConnectionString:{cs}");
+
+
+if (string.IsNullOrEmpty(cs) || string.IsNullOrEmpty(queueName))
+    throw new Exception("RabbitMq is not configured correctly");
+
+var retryCount = 0;
+const int maxRetries = 10;
+const int retryDelay = 5000; 
+
+while (retryCount < maxRetries)
+{
+    try
+    {
+        RabbitMqConsumer consumer = new(queueName, cs);
+        consumer.StartListening();
+        Console.WriteLine("Connected to RabbitMQ");
+        break;
+    }
+    catch (BrokerUnreachableException ex)
+    {
+        retryCount++;
+        Console.WriteLine($"Failed to connect to RabbitMQ. Retry {retryCount}/{maxRetries}. Exception: {ex.Message}");
+        Thread.Sleep(retryDelay);
+    }
+}
+
 
 Console.WriteLine("Starting RabbitMQ consumer...");
-consumer.StartListening();
