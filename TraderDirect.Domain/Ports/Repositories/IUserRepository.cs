f63bc0e972b4969fd5cd768dbe33272eeca4fedc
Mustafa Cankut Askan <m.askan@cashpoint.com>
2024-09-10 18:31:50 +0200

    Add user exists test

--- TraderDirect.Domain/Ports/Repositories/IUserRepository.cs
+++ TraderDirect.Domain/Ports/Repositories/IUserRepository.cs
@@ -8,8 +8,28 @@ using TraderDirect.Domain.Ports.Contracts;
 namespace TraderDirect.Domain.Ports.Repositories;
 public interface IUserRepository
 {
+    /// <summary>
+    /// Create user in database
+    /// </summary>
+    /// <param name="email">Email of the user</param>
+    /// <param name="cancellationToken">Cancellation token</param>
+    /// <returns>ID of the added user</returns>
     Task<int> CreateUserAsync(string email, CancellationToken cancellationToken);
 
+    /// <summary>
+    /// Check if user exists in database
+    /// </summary>
+    /// <param name="id">ID of the user</param>
+    /// <param name="cancellationToken">Cancellation token</param>
+    /// <returns>true if exist else false</returns>
     Task<bool> UserExists(int id, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Check if user exists in database
+    /// </summary>
+    /// <param name="email">Email of the user</param>
+    /// <param name="cancellationToken">Cancellation token</param>
+    /// <returns>true if exist else false</returns>
+    Task<bool> UserExists(string email, CancellationToken cancellationToken);
 }
 
