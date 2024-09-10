5b94de635a44a7b221d3d3f88e55598d4d064dce
Mustafa Cankut Askan <m.askan@cashpoint.com>
2024-09-10 18:05:06 +0200

    Add integration test.

---
+++ TraderDirect.Test/IntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using TraderDirect.Infrastructure.Databases.TraderDirectDb;
+
+namespace TraderDirect.Test.IntegrationTests;
+public class CustomWebApplicationFactory<TEntryPoint> : WebApplicationFactory<TEntryPoint> where TEntryPoint : class
+{
+    public SqliteConnection DbConnection;
+    private readonly string _connectionString = "DataSource=:memory:";
+
+    public CustomWebApplicationFactory()
+    {
+        DbConnection = new SqliteConnection(_connectionString);
+        DbConnection.Open();
+    }
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            var descriptor = services.SingleOrDefault(
+                d => d.ServiceType == typeof(DbContextOptions<TraderDirectDbContext>));
+            if (descriptor != null)
+            {
+                services.Remove(descriptor);
+            }
+
+            services.RemoveAll(typeof(DbContextOptions<TraderDirectDbContext>));
+            services.AddEntityFrameworkSqlite().AddDbContext<TraderDirectDbContext>(options =>
+            {
+                options.UseSqlite(DbConnection);
+            });
+
+            var sp = services.BuildServiceProvider();
+            using var scope = sp.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<TraderDirectDbContext>();
+            dbContext.Database.OpenConnection();
+            dbContext.Database.EnsureCreated();
+        });
+    }
+}
+
